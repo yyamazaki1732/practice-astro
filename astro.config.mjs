1181b0b3d4d809e59eb09d199a3e071eb9ad210d
YAMAZAKI <130333162+yyamazaki0410@users.noreply.github.com>
2023-04-24 10:09:04 +0900

    fix settings files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,30 @@
 import { defineConfig } from 'astro/config';
+import image from '@astrojs/image';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: 'entry.[hash].js',
+          chunkFileNames: 'chunks/chunk.[hash].js',
+          assetFileNames: 'assets/asset.[hash][extname]',
+        },
+      },
+    },
+  },
+  optimize: {
+    plugins: [
+      {
+        name: "css",
+        options: {
+          output: "/assets/css/styles.css",
+        },
+      },
+    ],
+  },
+  integrations: [image()],
+})
+
+

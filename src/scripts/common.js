27401fe634d11caab5a713cd893ead061484078a
YAMAZAKI <130333162+yyamazaki0410@users.noreply.github.com>
2023-04-24 10:11:24 +0900

    rename folders

---
+++ src/scripts/common.js
@@ -0,0 +1,27 @@
+const heading = document.getElementsByClassName(
+    "l-section__heading--secondary"
+);
+
+let count = 0;
+
+for (let i = 0; i < heading.length; i++) {
+    const content = heading[i].nextElementSibling;
+
+    heading[i].addEventListener("click", () => {
+        count++;
+        if (count % 2 == 0) {
+        toggleUp();
+    } else {
+        toggleDown();
+    }
+    });
+
+    function toggleUp() {
+    heading[i].classList.remove("is-active");
+    content.classList.remove("is-active");
+    }
+    function toggleDown() {
+    heading[i].classList.add("is-active");
+    content.classList.add("is-active");
+    }
+}

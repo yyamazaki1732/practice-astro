7d2ef6d32ef2865ab253fa172ee9b561cb38c0f4
YAMAZAKI <130333162+yyamazaki0410@users.noreply.github.com>
2023-08-13 08:21:57 +0900

    add: tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from "astro/config";
 import relativeLinks from "astro-relative-links";
 import image from "@astrojs/image";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), relativeLinks()],
+  integrations: [image(), relativeLinks(), tailwind()]
 });

a45b354a3fc4503f9819d597b0f49dd3bf0a71c7
YAMAZAKI <130333162+yyamazaki0410@users.noreply.github.com>
2023-09-25 22:17:02 +0900

    set for dev to kuroko

--- src/components/common/ReactButtom/Button.jsx
+++ src/components/common/ReactButtom/Button.jsx
@@ -2,7 +2,7 @@ import { ButtonText } from "./ButtonText";
 
 const Button = () => {
   return (
-    <div className="tw-grid tw-grid-cols-3 tw-gap-x-8">
+    <div className="tw-grid tw-grid-cols-3 tw-gap-x-2 tw-py-2">
       <ButtonText title={"会社名"} content={"アナイスカンパニー"} />
       <ButtonText title={"最寄り"} content={"表参道"} />
       <ButtonText title={"徒歩"} content={"約10分"} />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,60 @@ module.exports = {
     // # extend
     // ===============================================
     extend: {
+      spacing: {
+        6: "1.5rem", // 24px
+        6.5: "1.625rem", // 26px
+        8: "2rem", // 32px
+        8.5: "2.125rem", // 34px
+        10: "2.5rem", // 40px
+        10.5: "2.625rem", // 42px
+        12: "3rem", // 48px
+        12.5: "3.125rem", // 50px
+        14: "3.5rem", // 56px
+        14.5: "3.625rem", // 58px
+        16: "4rem", // 64px
+        16.5: "4.125rem", // 66px
+        18: "4.5rem", // 72px
+        18.5: "4.625rem", // 74px
+        22: "5.5rem", // 88px
+        22.5: "5.625rem", // 90px
+        26: "6.5rem", // 104px
+        26.5: "6.625rem", // 106px
+        30: "7.5rem", // 120px
+        30.5: "7.625rem", // 122px
+        34: "8.5rem", // 136px
+        34.5: "8.625rem", // 138px
+        38: "9.5rem", // 152px
+        38.5: "9.625rem", // 154px
+        42: "10.5rem", // 168px
+        42.5: "10.625rem", // 170px
+        46: "11.5rem", // 184px
+        46.5: "11.625rem", // 186px
+        50: "12.5rem", // 200px
+        50.5: "12.625rem", // 202px
+        54: "13.5rem", // 216px
+        54.5: "13.625rem", // 218px
+        58: "14.5rem", // 232px
+        58.5: "14.625rem", // 234px
+        62: "15.5rem", // 248px
+        62.5: "15.625rem", // 250px
+        66: "16.5rem", // 264px
+        66.5: "16.625rem", // 266px
+        70: "17.5rem", // 280px
+        70.5: "17.625rem", // 282px
+        74: "18.5rem", // 296px
+        74.5: "18.625rem", // 298px
+        78: "19.5rem", // 312px
+        78.5: "19.625rem", // 314px
+        82: "20.5rem", // 328px
+        82.5: "20.625rem", // 330px
+        86: "21.5rem", // 344px
+        86.5: "21.625rem", // 346px
+        90: "22.5rem", // 360px
+        90.5: "22.625rem", // 362px
+        94: "23.5rem", // 376px
+        94.5: "23.625rem", // 378px
+      },
       screens: {
         sm: "520px",
         // => @media (min-width: 640px) { ... }
@@ -28,20 +82,20 @@ module.exports = {
       // ===============================================
       // # themeの具体例
       // ===============================================
-      spacing: {
-        small: "10px",
-        medium: "20px",
-      },
-      padding: ({ theme }) => ({
-        btnSmall: theme("spacing.small"),
-        btnMedium: theme("spacing.medium"),
-      }),
-      backgroundSize: ({ theme }) => ({
-        auto: "auto",
-        cover: "cover",
-        contain: "contain",
-        ...theme("spacing"),
-      }),
+      // spacing: {
+      //   small: "10px",
+      //   medium: "20px",
+      // },
+      // padding: ({ theme }) => ({
+      //   btnSmall: theme("spacing.small"),
+      //   btnMedium: theme("spacing.medium"),
+      // }),
+      // backgroundSize: ({ theme }) => ({
+      //   auto: "auto",
+      //   cover: "cover",
+      //   contain: "contain",
+      //   ...theme("spacing"),
+      // }),
       // p-fv__pic > imgに適用
       // ===============================================
     },
@@ -79,11 +133,14 @@ module.exports = {
       },
     },
     fontSize: {
-      sm: ["14px", "20px"],
+      jt40: ["3rem", "100%"],
       base: ["16px", "24px"],
       lg: ["20px", "160%"],
       xl: ["24px", "32px"],
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    // ...
+  ],
 };
